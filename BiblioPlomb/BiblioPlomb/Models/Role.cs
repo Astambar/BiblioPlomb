f9f5460f5795df577dc0034dc117a354340701c2
Astambar <leuchartjason@gmail.com>
2025-01-17 15:16:47 +0100

    Ajout  Role  + Table association  UtilisateurRole

---
+++ BiblioPlomb/BiblioPlomb/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace BiblioPlomb.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = null!;
+
+        // Propriété de navigation pour la relation plusieurs-à-plusieurs
+        public ICollection<UtilisateurRole> UtilisateurRoles { get; set; }
+    }
+}
---
+++ BiblioPlomb/BiblioPlomb/Models/Utilisateur.cs
@@ -0,0 +1,16 @@
+﻿using BiblioPlomb.Models.BiblioPlomb.Models;
+
+namespace BiblioPlomb.Models
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string MotDePasse { get; set; }
+
+        // Propriété de navigation pour la relation plusieurs-à-plusieurs
+        public ICollection<UtilisateurRole> UtilisateurRoles { get; set; }
+    }
+}
