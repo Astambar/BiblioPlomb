7cfb391e4f01ff8bd1cd260fbf6773837798dcb9
TreDozier-hub <arnaud.dozier@gmail.com>
2025-01-17 15:59:02 +0100

    Crétion de GenreDTO

---
+++ BiblioPlomb/BiblioPlomb/DTO/GenreDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BiblioPlomb.DTO
+{
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+
+        //Clé étrangère
+        public int LivreId { get; set; }
+    }
+}
