94c860ca43a57787eb1b18b907624258e05706fb
Valentin-M.dev <valentin.mamalet@hotmail.fr>
2025-01-20 10:03:46 +0100

    Update Obj

--- BiblioPlomb/BiblioPlomb/obj/Debug/net9.0/BiblioPlomb.AssemblyInfo.cs
+++ BiblioPlomb/BiblioPlomb/obj/Debug/net9.0/BiblioPlomb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab43f2a59d9b91c76b96506b4b7e99bed7267dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712825098c885d8f2baa016bdc80b7ce0eeda429")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
