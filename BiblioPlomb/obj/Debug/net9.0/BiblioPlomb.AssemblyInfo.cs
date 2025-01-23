a789e9a936b2a28f5037c41a3ff17289d348eaca
ValentinM-dev <valentin.mamalet@hotmail.fr>
2025-01-23 18:10:15 +0100

    Regroupement final et préparation pour Swagge

--- BiblioPlomb/obj/Debug/net9.0/BiblioPlomb.AssemblyInfo.cs
+++ BiblioPlomb/obj/Debug/net9.0/BiblioPlomb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06f49aa2d6589fc2c79d4410a916b1045703727")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72db1538eead02d1cfbbe83c197af26d1b016bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiblioPlomb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
